7bc4a84c972398109e2265d647698e877e66a531
Kevin <kevin.wei20622@gmail.com>
2022-06-09 21:37:22 -0700

    added multiple body part tracking, collision

--- jspages/snake.js
+++ jspages/snake.js
@@ -1,6 +1,6 @@
-body=[]
-headx = 6;
-heady = 6;
+body=[[6, 6],[6, 7]]
+grid=[]
+
 prevx = -1;
 prevy = -1;
 
@@ -18,7 +18,16 @@ var smallHeight = 50;
 var nextTime = 0;  //holds off next move time
 var speedHz = 30;
 
+var temp = 1;
+
 function draw() {
+
+    if(temp == 1) {
+        for(i = 0; i < body.length; i++) {
+            fill(0);
+            rect(smallWidth*body[i][0], smallHeight*body[i][1], smallWidth, smallHeight, 10);
+        }
+    }
     
     for(var x = 0; x <= width; x += width/12) {
         for(var y = 0; y <= height; y += height/12) {
@@ -29,6 +38,7 @@ function draw() {
     }
 
     nextTime++;
+    key;
     document.addEventListener('keydown', function(event) {
         if(event.key.indexOf("Arrow") != -1) {
             event.preventDefault();
@@ -37,22 +47,51 @@ function draw() {
     })
     
     if(nextTime == speedHz) { //wait until counter hits this to update
+
+        coord = body.shift();
+        headx = coord[0];
+        heady = coord[1];
+
+        collision = checkCollision(headx, heady, key); //checks if snake ran into body or wall
+
+        if(collision) {
+            location.reload();
+        }
+
+        body.unshift([headx, heady]); //add the current head back since snake body needs to stay connected, so should only leave tail deleted
+
         nextTime = 0;
         moveHead(headx, heady, key);
         
         fill(0);
         rect(smallWidth*headx, smallHeight*heady, smallWidth, smallHeight, 10);
         
-        if(prevx != -1 && prevy != -1) {
+        if(body.length > 0) {
+            tail = body.pop();
             fill(255);
-            rect(smallWidth*prevx, smallHeight*prevy, smallWidth, smallHeight);
+            rect(smallWidth*tail[0], smallHeight*tail[1], smallWidth, smallHeight);
         }
+
+        body.unshift([headx, heady]);
 
     }
 
 
 
 
+}
+
+function checkCollision(x, y, key) {
+    switch(key) {
+        case "ArrowUp":
+            if(y == 0) return true;
+        case "ArrowDown":
+            if(y == 12) return true;
+        case "ArrowLeft":
+            if(x == 0) return true;
+        case "ArrowRight":
+            if(x == 12) return true;
+    }
 }
 
 function moveHead(x, y, key) {
