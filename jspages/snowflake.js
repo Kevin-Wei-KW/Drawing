4599afb7071effd1079f2db7c39a06f69844120b
Whatthehacky <69333576+Whatthehacky@users.noreply.github.com>
2022-06-14 15:47:30 -0700

    minor bug fixes;  added snowflake

---
+++ jspages/snowflake.js
@@ -0,0 +1,12 @@
+
+function setup() {
+    canvas = createCanvas(600, 600);
+    stroke(0);
+    frameRate(30);
+    background(255);
+    fill(0);
+}
+
+function draw() {
+    line(width/2, 0, width/2, height/4);
+}
