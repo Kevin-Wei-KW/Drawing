b4fe75feeb7cd2cfeb973954e6259b8a2951cf0f
Kevin Wei <69333576+Kevin-Wei-KW@users.noreply.github.com>
2022-11-10 22:41:22 +0000

    Add sketch Enhance UI

--- jspages/sketch.js
+++ jspages/sketch.js
@@ -1,30 +1,41 @@
 let y = 100;
 function setup() {
-    canvas = createCanvas(720, 400);
-    //canvas.parent('canvasBox');
-    stroke(0);
-    frameRate(60);
-    background(255);
+  canvas = createCanvas(720, 400);
+  //canvas.parent('canvasBox');
+  stroke(0);
+  frameRate(200);
+  background(255);
+  document.addEventListener('contextmenu', (evt) => {
+    evt.preventDefault();
+  })
+  noStroke();
+
 }
 
 function draw() {
-    if(mouseIsPressed) {
-        fill(0);   //fills shapes drawn later a certain colour
-    } else {
-        fill(255);
+
+  if (mouseIsPressed) {
+    if (mouseButton == LEFT) {
+      fill(0);   //fills shapes drawn later a certain colour
     }
+    if (mouseButton == RIGHT) {
+      fill(255);
+    }
+  } else {
+    return;
+  }
 
-    ellipse(mouseX, mouseY, 30, 30);
+  ellipse(mouseX, mouseY, 30, 30);
 
 
 
 
 
 
-    //Auto line
-    // y = y-1;
-    // if (y < 0) {
-    //     y = height;
-    // }
-    // line (0, y, width, y); // line(x1, y1, x2, y2) || line(x1, y1, z1, x2, y2, z2)
+  //Auto line
+  // y = y-1;
+  // if (y < 0) {
+  //     y = height;
+  // }
+  // line (0, y, width, y); // line(x1, y1, x2, y2) || line(x1, y1, z1, x2, y2, z2)
 }
